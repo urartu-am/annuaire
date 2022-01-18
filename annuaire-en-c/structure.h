79fa307988f11c25d94db335caace7a53080e8b7
unknown <mesrop.aghumyan@etu.univ-lyon1.fr>
2022-01-18 23:21:31 +0100

    création du l'unique header structure v 1.0

---
+++ annuaire-en-c/structure.h
@@ -0,0 +1,17 @@
+#ifndef STRUCTURE
+#define STRUCTURE
+
+struct Client
+{
+    char nom[50];
+    char prenom[50];
+    char ville[50];
+    char metier[50]
+    int cp[5];
+    int numero[10];
+};
+typedef Client client;
+
+
+
+#endif
